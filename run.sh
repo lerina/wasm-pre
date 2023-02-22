6bb59fec105bcc17235582240c8e430d82fbb494
Lerina J-Y Razafy <lerina.razafy@gmail.com>
2023-02-21 19:10:16 -0500

    starting with dt

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ set -ex
 
 wasm-pack build --target web --out-dir www/pkg $1 $2
 
-printf "\nNow serving on \n\t http://127.0.0.1:8080/html \n\n"
+printf "\nNow serving at \n\t http://127.0.0.1:8080/html \n\n"
 
 http -a 127.0.0.1 -p 8080 www
 
